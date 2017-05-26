eb404bdd37aa799c6ac04887e5f21fe1ebc0c44c
Brett <bretttucker@gmail.com>
2017-05-26 12:08:23 -0600

    Add ping service

---
+++ ping-service/src/main/java/com/abnormallydriven/swarm/Main.java
@@ -0,0 +1,22 @@
+package com.abnormallydriven.swarm;
+
+import spark.Request;
+import spark.Response;
+
+import java.util.UUID;
+
+import static spark.Spark.post;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String randomUUID = UUID.randomUUID().toString();
+
+        post("/ping", (Request req, Response res) -> {
+            res.header("Instance-UUID", randomUUID);
+            res.status(204);
+            return "";
+        });
+
+    }
+}
