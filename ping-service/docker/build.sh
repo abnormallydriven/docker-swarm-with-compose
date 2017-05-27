d09fba4bacff7da5f7913180b7ad17c221d81b26
Brett <bretttucker@gmail.com>
2017-05-27 17:59:42 -0600

    Update docker-compose file
    This will take advantage of some of the new swarm only compose options
    available to us in docker-compose version 3.

--- ping-service/docker/build.sh
+++ ping-service/docker/build.sh
@@ -6,3 +6,5 @@ cp -fr ../target/pingservice.jar pingservice.jar
 
 docker build -t abnormallydriven/pingservice:latest \
     -t us.gcr.io/$GCLOUD_DOCKER_REGISTRY/abnormallydriven/pingservice:latest .
+
+gcloud docker push -- us.gcr.io/$GCLOUD_DOCKER_REGISTRY/abnormallydriven/pingservice
